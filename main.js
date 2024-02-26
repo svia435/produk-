5162d23ae3eb115883d5380f8d556ea51e956341
svia435 <svia435@gmail.com>
2024-02-26 11:07:35 +0700

    menambah

--- main.js
+++ main.js
@@ -46,3 +46,17 @@ export async function ambilDaftarProduk() {
 export function formatAngka(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export async function  tambahProduk(nama, harga, stok) {
+  try {
+    const dokRef = await addDoc(collection(db, 'produk'),{
+      nama: nama,
+      harga: harga,
+      stok: stok
+    });
+    console.log('Berhasil menambah produk' + dokRef.id);
+  } catch (e) {
+    Console.log('Gagal menambah produk' + e);
+  }
+
+}
